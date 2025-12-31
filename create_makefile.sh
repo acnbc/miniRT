73153cd35e2cf5c83cd076916512a56e1417d597
jessica <jessicasoaresdev@gmail.com>
2025-12-31 13:14:25 -0300

    tratando minilibx no script de criacao do makefile

--- create_makefile.sh
+++ create_makefile.sh
@@ -5,6 +5,9 @@ _NAME=''
 _DIR_LIB=''
 _DIR_INCLUDE=''
 _LIBFT=''
+_INCLUDE_MINILIBX=''
+_DIR_LIBX=''
+_MINILIBX=''
 _DIR_OBJ=''
 _ARRAY_DIR=''
 _FILES=''
@@ -15,15 +18,17 @@ MAKE_EXISTIS=$(find . -type f | grep -F ./Makefile | wc -l)
 _UPDATE='N'
 
 
-if ([ $1 != "-y" ] && [ $1 != "-Y" ]) || [ $1 == "--help" ]; then
+shopt -s nocasematch
+if [ -n "$1" ] && [[ $1 != "-y" ]] || [[ $1 == "--help" ]]; then
 	echo "Usage: ./create_makefile.sh [options]"
 	echo "Options:"
 	echo "  --help	Show this help message and exit"
 	echo "  -y, -Y	Automatically update the Makefile if it exists"
 	exit 0
 fi
+shopt -u nocasematch
 
-if [ $1 == "-y" ] || [ $1 == "-Y" ]; then
+if [ "$1" == "-y" ] || [ "$1" == "-Y" ]; then
 	_UPDATE='Y'
 elif [ $MAKE_EXISTIS -eq 1 ] && [ "$_UPDATE" != "Y" ] && [ "$_UPDATE" != "y" ]; then
 	read -p "Makefile already exists. Do you want to update it? (Y/N): " _UPDATE
@@ -31,10 +36,17 @@ fi
 
 if ([ "$_UPDATE" == "N" ] && [ "$_UPDATE" == "n" ]) || [ $MAKE_EXISTIS -eq 0 ]; then
 	read -p "Enter the program name: " _NAME
-	read -p "Enter the program compiler: " _CC
+	read -p "Enter the program compiler (default: cc): " _CC
 	read -p "Enter the include directory (default: include/): " _DIR_INCLUDE
 	read -p "Enter the libft directory (default: lib/): " _DIR_LIB
 	read -p "Enter the libft file (default: libft.a): " _LIBFT
+
+	read -p "Include minilibx? (y/n): " _INCLUDE_MINILIBX
+	if [ "${_INCLUDE_MINILIBX,,}" == "y" ]; then
+		read -p "Enter the minilibx directory (default: minilibx/): " _DIR_LIBX
+		read -p "Enter the minilibx file (default: libmlx_Linux.a): " _MINILIBX
+	fi
+
 	read -p "Enter the object files directory (default: obj/): " _DIR_OBJ
 	read -p "Enter additional flags for compiling the program: " _ADD_FLAGS_COMPILE_PROGRAM
 	read -p "Enter additional flags for compiling object files: " _ADD_FLAGS_COMPILE_OBJS
@@ -44,6 +56,8 @@ else
 	_DIR_INCLUDE=$(cat Makefile | grep -F 'DIR_INCLUDE =' | sed -E 's/DIR_INCLUDE =\s*(\.\/)?(.*)/\2/')
 	_DIR_LIB=$(cat Makefile | grep -F 'DIR_LIB =' | sed -E 's/DIR_LIB =\s*(\.\/)?(.*)/\2/')
 	_LIBFT=$(cat Makefile | grep -F 'LIBFT =' | sed -E 's/LIBFT =\s*\$\(DIR_LIB\)\/(.*)/\1/')
+	_DIR_LIBX=$(cat Makefile | grep -F 'DIR_LIBX =' | sed -E 's/DIR_LIBX =\s*(\.\/)?(.*)/\2/')
+	_MINILIBX=$(cat Makefile | grep -F 'MINILIBX =' | sed -E 's/MINILIBX =\s*\$\(DIR_LIBX\)\/(.*)/\1/')
 	_DIR_OBJ=$(cat Makefile | grep -F 'DIR_OBJ =' | sed -E 's/DIR_OBJ =\s*(\.\/)?(.*)/\2/')
 	_ADD_FLAGS_COMPILE_PROGRAM=$(cat Makefile | grep -F 'ADD_FLAGS_COMPILE_PROGRAM =' | sed -E 's/ADD_FLAGS_COMPILE_PROGRAM =\s*(.*)/\1/')
 	_ADD_FLAGS_COMPILE_OBJS=$(cat Makefile | grep -F 'ADD_FLAGS_COMPILE_OBJS =' | sed -E 's/ADD_FLAGS_COMPILE_OBJS =\s*(.*)/\1/')
@@ -67,12 +81,25 @@ if [ -z "$_LIBFT" ]; then
 	_LIBFT="libft.a"
 fi
 
+if [ "${_INCLUDE_MINILIBX,,}" == "y" ] && [ -z "$_DIR_LIBX" ]; then
+	_DIR_LIBX="minilibx"
+fi
+
+if [ "${_INCLUDE_MINILIBX,,}" == "y" ] && [ -z "$_MINILIBX" ]; then
+	_MINILIBX="libmlx_Linux.a"
+fi
+
 if [ -z "$_DIR_OBJ" ]; then
 	_DIR_OBJ="obj"
 fi
 
 
 REGEX_PATH="(^\.\/("$_DIR_INCLUDE"|"$_DIR_LIB"|"$_DIR_OBJ"|\.))"
+
+if [ -n "$_DIR_LIBX" ]; then
+	REGEX_PATH="(^\.\/("$_DIR_INCLUDE"|"$_DIR_LIB"|"$_DIR_OBJ"|"$_DIR_LIBX"|\.))"
+fi
+
 
 _ARRAY_DIR=$(find . -type d | sed -E "/$REGEX_PATH|(^\.$)|(^\.\/\s$)/d" | sed -E 's/\.(.*)/\t\$(DIR_OBJ)\1 \\/' | sed -E '$s/(.*)\s\\/\1/')
 _FILES=$(find . -type f | grep -F .c | sed -E "/$REGEX_PATH|(^\.\/\s$)/d" | sed -E 's/\.\/(.*)/\t\1 \\/' | sed -E '$s/(.*)\s\\/\1/')
@@ -83,6 +110,8 @@ NAME="NAME = $_NAME"
 DIR_INCLUDE="DIR_INCLUDE = ./$_DIR_INCLUDE"
 DIR_LIB="DIR_LIB = ./$_DIR_LIB"
 LIBFT="LIBFT = "'$(DIR_LIB)'"/$_LIBFT"
+DIR_LIBX="DIR_LIBX = ./$_DIR_LIBX"
+MINILIBX="MINILIBX = "'$(DIR_LIBX)'"/$_MINILIBX"
 DIR_OBJ="DIR_OBJ = ./$_DIR_OBJ"
 DIR='DIR = $(DIR_OBJ) \
 '"$_ARRAY_DIR"
@@ -98,6 +127,12 @@ echo -e "$NAME\n" >> Makefile
 echo -e "$DIR_INCLUDE\n" >> Makefile
 echo -e "$DIR_LIB\n" >> Makefile
 echo -e "$LIBFT\n" >> Makefile
+
+if [ -n "$_DIR_LIBX" ] && [ -n "$_MINILIBX" ]; then
+	echo -e "$DIR_LIBX\n" >> Makefile
+	echo -e "$MINILIBX\n" >> Makefile
+fi
+
 echo -e "$DIR_OBJ\n" >> Makefile
 echo -e "$DIR\n" >> Makefile
 echo -e "$FILES\n" >> Makefile
@@ -116,8 +151,20 @@ all: $(NAME)
 
 $(NAME): $(OBJS)
 	@ make -C $(DIR_LIB)
-	@ $(CC) -Wall -Wextra -Werror -g $(OBJS) $(LIBFT) -o $(NAME) $(ADD_FLAGS_COMPILE_PROGRAM)
-	@ clear
+' >> Makefile
+
+if [ -n "$_DIR_LIBX" ] && [ -n "$_MINILIBX" ]; then
+echo -n '	@ make -C $(DIR_LIBX)
+	@ $(CC) -Wall -Wextra -Werror -g $(OBJS) $(LIBFT) $(MINILIBX) -o $(NAME) $(ADD_FLAGS_COMPILE_PROGRAM)
+' >> Makefile
+
+else
+echo -n '	@ $(CC) -Wall -Wextra -Werror -g $(OBJS) $(LIBFT) -o $(NAME) $(ADD_FLAGS_COMPILE_PROGRAM)
+' >> Makefile
+
+fi
+
+echo -n '	@ clear
 	@ echo "$(GREEN)Compiled successfully!$(RESET)"
 
 $(DIR_OBJ)/%.o: %.c | $(DIR_OBJ)
@@ -131,13 +178,27 @@ $(DIR_OBJ):
 clean:
 	@ rm -rf $(DIR_OBJ)
 	@ make clean -C $(DIR_LIB)
-	@ clear
+' >> Makefile
+
+if [ -n "$_DIR_LIBX" ] && [ -n "$_MINILIBX" ]; then
+echo -n '	@ make clean -C $(DIR_LIBX)
+' >> Makefile
+fi
+
+echo -n '	@ clear
 	@ echo "$(RED)Object files removed!$(RESET)"
 
 fclean:
 	@ rm -rf $(DIR_OBJ) $(NAME)
 	@ make fclean -C $(DIR_LIB)
-	@ clear
+' >> Makefile
+
+if [ -n "$_DIR_LIBX" ] && [ -n "$_MINILIBX" ]; then
+echo -n '	@ make clean -C $(DIR_LIBX)
+' >> Makefile
+fi
+
+echo -n '	@ clear
 	@ echo "$(RED)Object files and $(NAME) removed!$(RESET)"
 
 re: fclean all
@@ -145,5 +206,8 @@ re: fclean all
 run: all clean
 	./$(NAME)
 
-.PHONY: all clean fclean re run $(NAME)
+update:
+	bash create_makefile.sh -y
+
+.PHONY: all clean fclean re run update $(NAME)
 ' >> Makefile
