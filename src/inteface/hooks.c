4c681c3cc52ebb6eb2cb7616dddd85fef05dbc4d
jessica <jessicasoaresdev@gmail.com>
2026-02-16 20:37:55 -0300

    configurando hooks

---
+++ src/inteface/hooks.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hooks.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jessica <jessica@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/16 17:35:44 by jessica           #+#    #+#             */
+/*   Updated: 2026/02/16 19:11:53 by jessica          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/miniRT.h"
+
+static int	key_hook(int keycode, void *param);
+static int	hook(void *param);
+
+void	init_hooks(t_scene *scene)
+{
+	mlx_key_hook(scene->window->win_ptr, &key_hook, scene);
+	mlx_hook(scene->window->win_ptr, DestroyNotify, NoEventMask, &hook, scene);
+	mlx_loop(scene->window->mlx_ptr);
+}
+
+static int	key_hook(int keycode, void *param)
+{
+	t_scene	*scene;
+
+	scene = (t_scene *)param;
+	if (keycode == XK_Escape)
+	{
+		mlx_loop_end(scene->window->mlx_ptr);
+		free_scene(&scene);
+		exit(0);
+	}
+	return (0);
+}
+
+static int	hook(void *param)
+{
+	t_scene	*scene;
+
+	scene = (t_scene *)param;
+	mlx_loop_end(scene->window->mlx_ptr);
+	free_scene(&scene);
+	exit(0);
+	return (0);
+}
