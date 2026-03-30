66ec329b7c6091abab780ffa99e9d12fbc78ed95
jessica <jessicasoaresdev@gmail.com>
2026-03-30 01:50:43 -0300

    refatorando funcoes de criacao de elementos

--- src/parsing/object/sgl_object.c
+++ src/parsing/object/sgl_object.c
@@ -6,91 +6,84 @@
 /*   By: jessica <jessica@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/01 17:53:27 by jessica           #+#    #+#             */
-/*   Updated: 2026/03/17 23:32:47 by jessica          ###   ########.fr       */
+/*   Updated: 2026/03/30 01:50:17 by jessica          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/miniRT.h"
 
+static void	error_create_element(char ***infos, t_msg_error error);
+
 void	create_amb_light(t_scene *scene, char ***infos, int index)
 {
 	t_msg_error	error;
+	t_amb_light	*amb_light;
 
 	scene->amb_light = (t_amb_light *)ft_calloc(1, sizeof(t_amb_light));
 	if (!scene->amb_light)
+		error_create_element(infos, ERR_MALLOC);
+	amb_light = scene->amb_light;
+	amb_light->id = A;
+	amb_light->light_ratio = ft_atod((*infos)[index]);
+	if (amb_light->light_ratio < 0 || amb_light->light_ratio > 1)
+		error_create_element(infos, ERR_RANGE);
+	if (!*infos)
 	{
-		ft_split_free(infos);
-		exit_error(ERR_MALLOC, NULL);
+		error = get_coolors(&amb_light->colors, *infos, index + 1);
+		if (error)
+			error_create_element(infos, error);
 	}
-	scene->amb_light->id = A;
-	scene->amb_light->light_ratio = ft_atod((*infos)[index]);
-	if (scene->amb_light->light_ratio < 0 || scene->amb_light->light_ratio > 1)
-	{
-		ft_split_free(infos);
-		exit_error(ERR_RANGE, NULL);
-	}
-	error = get_coolors(&scene->amb_light->colors, *infos, index + 1);
-	if (error)
-	{
-		ft_split_free(infos);
-		exit_error(error, NULL);
-	}
+	else
+		amb_light->colors = (t_rgb){255, 255, 255};
 }
 
 void	create_camera(t_scene *scene, char ***infos, int index)
 {
 	t_msg_error	error;
+	t_camera	*camera;
 
 	scene->camera = (t_camera *)ft_calloc(1, sizeof(t_camera));
 	if (!scene->camera)
-	{
-		ft_split_free(infos);
-		exit_error(ERR_MALLOC, NULL);
-	}
-	scene->camera->id = C;
-	scene->camera->view_point = get_coord(*infos, index, false);
-	scene->camera->orientation_vector = get_coord(*infos, index + 1, true);
-	error = valid_tuple(scene->camera->view_point);
-	if (!error)
-		error = valid_tuple(scene->camera->orientation_vector);
+		error_create_element(infos, ERR_MALLOC);
+	camera = scene->camera;
+	camera->id = C;
+	camera->view_point = get_coord(*infos, index, false);
+	camera->orientation_vector = get_coord(*infos, index + 1, true);
+	error = valid_tuple(camera->view_point);
 	if (error)
-	{
-		ft_split_free(infos);
-		exit_error(error, NULL);
-	}
-	scene->camera->field_of_view = ft_atoi((*infos)[index + 2]);
-	if (scene->camera->field_of_view < 0 || scene->camera->field_of_view > 180)
-	{
-		ft_split_free(infos);
-		exit_error(ERR_RANGE, NULL);
-	}
+		error_create_element(infos, error);
+	error = valid_tuple(camera->orientation_vector);
+	if (error)
+		error_create_element(infos, error);
+	camera->field_of_view = ft_atoi((*infos)[index + 2]);
+	if (camera->field_of_view < 0 || camera->field_of_view > 180)
+		error_create_element(infos, ERR_RANGE);
 }
 
 void	create_light(t_scene *scene, char ***infos, int index)
 {
 	t_msg_error	error;
+	t_light		*light;
 
 	scene->light = (t_light *)ft_calloc(1, sizeof(t_light));
 	if (!scene->light)
-	{
-		ft_split_free(infos);
-		exit_error(ERR_MALLOC, NULL);
-	}
-	scene->light->id = L;
-	scene->light->light_point = get_coord(*infos, index, false);
-	error = valid_tuple(scene->light->light_point);
-	if (!error)
-	{
-		scene->light->brightness = ft_atod((*infos)[index + 1]);
-		if (scene->light->brightness < 0 || scene->light->brightness > 1)
-		{
-			ft_split_free(infos);
-			exit_error(ERR_RANGE, NULL);
-		}
-		error = get_coolors(&scene->light->colors, *infos, index + 2);
-	}
-	if (!error)
-		return ;
+		error_create_element(infos, ERR_MALLOC);
+	light = scene->light;
+	light->id = L;
+	light->point = get_coord(*infos, index, false);
+	error = valid_tuple(light->point);
+	if (error)
+		error_create_element(infos, error);
+	light->brightness = ft_atod((*infos)[index + 1]);
+	if (light->brightness < 0 || light->brightness > 1)
+		error_create_element(infos, ERR_RANGE);
+	error = get_coolors(&light->colors, *infos, index + 2);
+	if (error)
+		error_create_element(infos, error);
+}
+
+static void	error_create_element(char ***infos, t_msg_error error)
+{
 	ft_split_free(infos);
 	exit_error(error, NULL);
 }
