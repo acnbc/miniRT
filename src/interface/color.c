5c78e7e749658431ef5a0433b8fc921cb83636b4
jessica <jessicasoaresdev@gmail.com>
2026-03-30 03:33:08 -0300

    correcao de erros

--- src/interface/color.c
+++ src/interface/color.c
@@ -6,7 +6,7 @@
 /*   By: jessica <jessica@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 17:18:47 by jessica           #+#    #+#             */
-/*   Updated: 2026/03/30 01:06:14 by jessica          ###   ########.fr       */
+/*   Updated: 2026/03/30 03:32:28 by jessica          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ unsigned int	convert_color(const t_tuple *tuple)
 	unsigned int	color;
 	t_rgb			rgb;
 
-	rgb.r = fmax(0.0, fmin(tuple->x, 1.0)) * 255;
-	rgb.g = fmax(0.0, fmin(tuple->y, 1.0)) * 255;
-	rgb.b = fmax(0.0, fmin(tuple->z, 1.0)) * 255;
+	rgb.r = (unsigned char)fmax(0.0, fmin(tuple->x * 255, 255));
+	rgb.g = (unsigned char)fmax(0.0, fmin(tuple->y * 255, 255));
+	rgb.b = (unsigned char)fmax(0.0, fmin(tuple->z * 255, 255));
 	color = 0;
 	color += (int)rgb.b;
 	color += ((int)rgb.g << 8);
--- src/math/tuple_operations.c
+++ src/math/tuple_operations.c
@@ -6,7 +6,7 @@
 /*   By: jessica <jessica@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/28 21:59:30 by jessica           #+#    #+#             */
-/*   Updated: 2026/03/28 22:09:29 by jessica          ###   ########.fr       */
+/*   Updated: 2026/03/30 03:31:05 by jessica          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	tuple_addition(t_tuple *result, const t_tuple *a, const t_tuple *b)
 	result->x = a->x + b->x;
 	result->y = a->y + b->y;
 	result->z = a->z + b->z;
+	result->is_point = 1;
 }
 
 void	tuple_multiplication(t_tuple *result, const t_tuple *a,
@@ -25,6 +26,7 @@ void	tuple_multiplication(t_tuple *result, const t_tuple *a,
 	result->x = a->x * b->x;
 	result->y = a->y * b->y;
 	result->z = a->z * b->z;
+	result->is_point = 1;
 }
 
 void	tuple_scalar_multiplication(t_tuple *result, const t_tuple *color,
@@ -33,4 +35,5 @@ void	tuple_scalar_multiplication(t_tuple *result, const t_tuple *color,
 	result->x = color->x * scalar;
 	result->y = color->y * scalar;
 	result->z = color->z * scalar;
+	result->is_point = 1;
 }
