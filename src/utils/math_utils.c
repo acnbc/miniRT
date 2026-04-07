13a51dcf6c442af61dfeed8d80562385c229ee3f
jessica <jessicasoaresdev@gmail.com>
2026-04-07 01:07:18 -0300

    criacao da funcao init_scaling_matrix

--- src/utils/math_utils.c
+++ src/utils/math_utils.c
@@ -6,7 +6,7 @@
 /*   By: jessica <jessica@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/02 12:03:49 by anogueir          #+#    #+#             */
-/*   Updated: 2026/03/21 15:06:50 by jessica          ###   ########.fr       */
+/*   Updated: 2026/04/07 00:31:15 by jessica          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,13 @@ bool	is_equal(double a, double b)
 	if (fabs(a - b) < EPSILON)
 		return (true);
 	return (false);
+}
+
+void	init_scaling_matrix(t_matrix *scale_mat, double x, double y, double z)
+{
+	init_identity_matrix(scale_mat, 4);
+	scale_mat->m_4x4[0] = x;
+	scale_mat->m_4x4[5] = y;
+	scale_mat->m_4x4[10] = z;
+	scale_mat->m_4x4[15] = 1.0;
 }
